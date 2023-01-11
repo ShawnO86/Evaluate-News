a1f643da8c185bd0b0745337417e71fc702e3635
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-01-11 12:31:21 -0600

    webpack production file

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,15 +25,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
         new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
