30ed67a95abe97e9efc633d437a58807a76ba3dc
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-01-11 13:17:21 -0600

    setup apiKey & dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,32 @@
-var path = require('path')
-const express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+var path = require('path');
+const express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const dotenv = require('dotenv');
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 // to use json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // to use url encoded values
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 
-app.use(express.static('dist'))
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-})
+});
 
-app.get('/newsEval', function (req, res) {
-
-})
+app.get('/apiKey', function (req, res) {
+  res.send(apiKey);
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8081!')
-})
+    console.log('Example app listening on port 8080!')
+});
