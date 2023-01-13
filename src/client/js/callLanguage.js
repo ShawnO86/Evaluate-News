4c879842aada5d66b7c30b3b4e0a6f980e4cc3cc
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-01-13 10:54:34 -0600

    ui update its own file

--- src/client/js/callLanguage.js
+++ src/client/js/callLanguage.js
@@ -1,5 +1,5 @@
 import { postData } from "./postData";
-import { getData } from "./getData";
+import { updateUI } from "./uiUpdate";
 
 async function checkLanguage(text, apiKey) {
   const req = await fetch(`https://api.meaningcloud.com/sentiment-2.1?lang=en&txt=${text}&key=${apiKey}`, {
@@ -22,23 +22,6 @@ async function checkLanguage(text, apiKey) {
 } catch (e) {
     console.log("error", e)
 }
-};
-
-const updateUI = async () => {
-  //added the http://localhost:3000/route so I can run dev mode and express server 
-  const currentData = await getData('http://localhost:3000/getData');
-  const display = document.getElementById('results');
-  try {
-      display.innerHTML = `<ul>
-      <li>Text: ${currentData.text}</li>
-      <li>Agreement: ${currentData.agreement}</li>
-      <li>Confidence: ${currentData.confidence}</li>
-      <li>Irony: ${currentData.irony}</li>
-      <li>Subjectivity: ${currentData.subjectivity}</li>
-      </ul>`;
-  } catch (e) {
-      console.log("error", e)
-  }
 };
 
 export {checkLanguage};
---
+++ src/client/js/uiUpdate.js
@@ -0,0 +1,20 @@
+import { getData } from "./getData";
+
+const updateUI = async () => {
+    //added the http://localhost:3000/route so I can run dev mode and express server 
+    const currentData = await getData('http://localhost:3000/getData');
+    const display = document.getElementById('results');
+    try {
+        display.innerHTML = `<ul>
+        <li>Text: ${currentData.text}</li>
+        <li>Agreement: ${currentData.agreement}</li>
+        <li>Confidence: ${currentData.confidence}</li>
+        <li>Irony: ${currentData.irony}</li>
+        <li>Subjectivity: ${currentData.subjectivity}</li>
+        </ul>`;
+    } catch (e) {
+        console.log("error", e)
+    }
+  };
+
+  export { updateUI }
